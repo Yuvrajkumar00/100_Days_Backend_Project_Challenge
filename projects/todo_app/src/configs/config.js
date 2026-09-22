8f1a354659ff2758ccaa7f76249c201ecee88488
= <=>
2026-09-23 02:42:13 +0530

    feat(auth): add user authentication methods and update config

--- projects/todo_app/src/configs/config.js
+++ projects/todo_app/src/configs/config.js
@@ -14,10 +14,30 @@ if(!process.env.MONGODB_URI) {
     throw new Error("MONGODB_URI is not defined in environment variables");
 }
 
-const confi = {
+if(!process.env.ACCESS_TOKEN_SECRET) {
+    throw new Error("ACCESS_TOKEN_SECRET is not defined in environment variables")
+}
+
+if(!process.env.ACCESS_TOKEN_EXPIRY) {
+    throw new Error("ACCESS_TOKEN_EXPIRY is not defined in environment variables")
+}
+
+if(!process.env.REFRESH_TOKEN_SECRET) {
+    throw new Error("REFRESH_TOKEN_SECRET is not defined in environment variables")
+}
+
+if(!process.env.REFRESH_TOKEN_EXPIRY) {
+    throw new Error("REFRESH_TOKEN_EXPIRY is not defined in environment variables")
+}
+
+const config = {
     PORT: process.env.PORT,
     BASE_URL: process.env.BASE_URL,
     MONGODB_URI: process.env.MONGODB_URI,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
 }
 
-export default confi;
+export default config;
--- projects/todo_app/src/models/user.models.js
+++ projects/todo_app/src/models/user.models.js
@@ -1,5 +1,9 @@
 import mongoose, {Schema} from "mongoose";
 import {AvailableUserRoles, UserRolesEnum} from "../utils/constants.js";
+import crypto from "crypto";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../configs/config.js";
 
 const userSchema = new Schema({
     name: {
@@ -33,8 +37,57 @@ const userSchema = new Schema({
     },
     verificationToken: {
         type: String,
+    },
+    verificationTokenExpiry: {
+        type: Date,
     }
 
 }, {timestamps: true});
+
+userSchema.pre("save", async function(next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        config.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: config.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+};
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            role: this.role,
+        },
+        config.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: config.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+};
+
+userSchema.methods.generateTemporaryToken = function() {
+    const unHashedToken = crypto.randomBytes(32).toString("hex");
+    const hashedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex");
+    const tokenExpiry = Date.now() + (20*60*1000) //20min
+
+    return {unHashedToken, hashedToken, tokenExpiry}
+}
+
 
 export const User = mongoose.model("User", userSchema);
