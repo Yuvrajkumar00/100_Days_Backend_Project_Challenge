0b498f3ed97294d937da4dc4b9fc8a3af2058afc
= <=>
2026-09-22 21:45:58 +0530

    feat(middleware): add error, multer and validation middleware

---
+++ projects/todo_app/src/middlewares/globalErrorHandler.middlewares.js
@@ -0,0 +1,13 @@
+import {ApiError} from "../utils/api-errors.js";
+import { ApiResponse } from "../utils/api-response.js";
+
+export const globalErrorHandler = (error, req, res, next) {
+
+    if(!(error instanceof ApiError)) {
+        return res.status(500).json(new ApiResponse(500, error.message ?? "Internal server error", null))
+    }
+
+    return res.status(error.statusCode || 500).json(new ApiResponse(error.statusCode || 500, error.message, null));
+}
+
+export {globalErrorHandler};
